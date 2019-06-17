350253e42b96b6a8d5b38a7ed9639b23f3600d5b
Nguyen Huy Hung <hungnh103@gmail.com>
2019-06-17 16:40:25 +0700

    Modify examples for part 2: Variables

--- 02. Ruby - Variables/4_local_variable.rb
+++ 02. Ruby - Variables/4_local_variable.rb
@@ -1,26 +1,24 @@
 color = "Red"
-def method1 
+
+def method1
   color = 192
-  puts("Color Value in method1 : ", color)
-end 
+  puts "Color value in method1: #{color}"
+end
 
 def method2
   color = 255
-  puts("Color Value method2: ", color)
+  puts "Color value in method2: #{color}"
 end
 
-method1 
-method2  
 method1
-puts("Color Value outside methods : " + color)
+method2
+method1
+puts("Color value outside methods : " + color)
 
 #Result
 =begin
-Color Value in method1 :
-192
-Color Value method2:
-255
-Color Value in method1 :
-192
-Color Value outside methods : Red
+Color value in method1: 192
+Color value in method2: 255
+Color value in method1: 192
+Color value outside methods : Red
 =end
